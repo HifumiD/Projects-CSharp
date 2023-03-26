98a427f6c686a44f05d88296c829325619c8f8b1
HifumiD <124528123+HifumiD@users.noreply.github.com>
2023-03-26 19:30:16 +0200

    Add files via upload

---
+++ Currency Converter/dbo.Currency_Master.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Currency_Master] (
+    [Id]           INT NOT NULL,
+    [Amount]      FLOAT (53)    NULL,
+    [CurrencyName] NVARCHAR(50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Currency Converter/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Currency_Master]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Ammount] FLOAT NULL, 
+    [CurrencyName] NVARCHAR(50) NULL
+	PRIMARY KEY CLUSTERED ([Id] ASC)
+)
